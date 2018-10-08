213c374d5e20952d4a82176818c4edc74f50adaa
unknown <marc@marchuang-NB1.tencent.com>
2018-10-09 00:48:22 +0800

    add trained model

--- tests/test_dag.py
+++ tests/test_dag.py
@@ -1,5 +1,16 @@
 from wsdetect import dag
+from wsdetect.settings import RESOURCE_DIR
 
 
 def test_main():
     dag.train()
+
+
+def test_predict():
+    img1 = RESOURCE_DIR / 'data' / 'train' / 'normal' / '1537430103356.jpg'
+    img2 = RESOURCE_DIR / 'data' / 'train' / 'white' / '1537430102405.jpg'
+    model_file = RESOURCE_DIR / 'models' / 'cnn.h5'
+
+
+    print(dag.predict(model_file, img1))
+    print(dag.predict(model_file, img2))
--- wsdetect/dag.py
+++ wsdetect/dag.py
@@ -1,13 +1,22 @@
-from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from wsdetect.settings import RESOURCE_DIR
 from wsdetect import cnn
-from keras.callbacks import TensorBoard
+from keras.callbacks import TensorBoard, EarlyStopping
 from pathlib import Path
 import shutil
+import numpy as np
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+from keras.models import load_model
 
 
 
 def train():
+
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    set_session(sess)
 
     tb_dir = Path('e:/tmp/wsdetect')
     if tb_dir.exists():
@@ -30,7 +39,11 @@ def train():
         batch_size=32,
         class_mode='categorical')
 
-    cb = TensorBoard(log_dir=tb_dir.absolute().as_posix(), histogram_freq=1, write_graph=True, write_images=True)
+    callbacks = [
+        TensorBoard(log_dir=tb_dir.absolute().as_posix(), histogram_freq=0, write_graph=True, write_images=True),
+        EarlyStopping(monitor='val_acc', min_delta=0.0001, patience=5, verbose=2, mode='auto')
+    ]
+
     model = cnn.build(input_shape=(96, 54, 3))
     model.fit_generator(
         train_generator,
@@ -39,8 +52,19 @@ def train():
         verbose=2,
         validation_data=validation_generator,
         validation_steps=800,
-        callbacks=[cb]
+        callbacks=callbacks
     )
 
     print('finish training, save the model')
     model.save((RESOURCE_DIR / 'models' / 'cnn.h5').absolute().as_posix())
+
+
+def predict(model_file, image_file):
+
+    model = load_model(Path(model_file).absolute().as_posix())
+    img = load_img(Path(image_file).absolute().as_posix(), target_size=(96, 54))
+    img_tensor = np.expand_dims(img_to_array(img), axis=0) / 255.0
+
+    probs = model.predict(img_tensor, verbose=2)
+    return probs.argmax(axis=-1)[0], probs.max()
+
